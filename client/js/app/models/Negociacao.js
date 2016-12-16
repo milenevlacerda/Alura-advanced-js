0dbaaa8c45242d9c93960dc8f6efae2430a0473d
Milene <milenelacerda@icloud.com>
2016-12-16 00:57:10 -0200

    Atualizando code

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,9 +1,12 @@
-
 class Negociacao {
 
-    constructor() {
-        this.data;
-        this.quantidade;
-        this.valor;
+    constructor( data, quantidade, valor ) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    obtemVolume() {
+        return this.quantidade*this.valor;
     }
 }
