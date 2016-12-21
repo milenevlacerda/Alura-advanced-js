623d2937acfe0d3fb8e12946fb282ebc3f48de04
Milene <milenelacerda@icloud.com>
2016-12-21 18:12:59 -0200

    Atualizando arquivos conforme final do módulo I de JavaScript avançado

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,10 +7,13 @@ class NegociacaoController {
         this._inputQuantidade = $( '#quantidade' );
         this._inputValor = $( '#valor' );
         this._listaNegociacoes = new ListaNegociacao();
-        this._negociacoesView = new NegociacoesView( $( '#negociacoesView' ));
 
+        this._negociacoesView = new NegociacoesView( $( '#negociacoesView' ));
         this._negociacoesView.update( this._listaNegociacoes );
 
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView( $('#mensagemView') );
+        this._mensagemView.update( this._mensagem );
     }
 
     adiciona( event ) {
@@ -18,7 +21,10 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona( this._criaNegociacao() );
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update( this._mensagem );
         this._negociacoesView.update( this._listaNegociacoes );
+
         this._limpaFormulario();
 
     }
